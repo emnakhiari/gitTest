3f8855688a969ce54e27c7bad9b2050db6e0a3af
emna khiari <khiariemna2@gmail.com>
2024-03-25 09:42:15 +0100

    modification2

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -37,7 +37,7 @@ public class User implements Serializable {
 	private String firstName; 
 	
 	private String lastName;
-	
+	private String lastName2;
 	@Temporal(TemporalType.DATE)
 	private Date dateNaissance;
 	
